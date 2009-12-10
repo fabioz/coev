52b56ca218957e0edbc290654c0f9f0e0bc6bcf5
lxnt <lxnt@localhost>
2009-12-10 18:09:41 +0300

    build cqsl and pull pylons dependencies via the example

--- build.sh
+++ build.sh
@@ -16,12 +16,13 @@ $PREFIX/bin/python ez_setup.py || exit 1
 ( cd python-coewsgi && ${PREFIX}/bin/python setup.py install ) || exit 1
 ( cd python-psycoev && rm -rf build && ./build.sh ) || exit 1
 ( cd python-evmemcached && ${PREFIX}/bin/python setup.py install ) || exit 1
+( cd cqsl && ${PREFIX}/bin/python setup.py install ) || exit 1
 
 # this depends on Pylons which depends on easyinstall which depends on python
 # which we just installed so ... the commands below will pull Paste, Pylons
 # and the rest of stuff needed for a Pylons project
 #
-#( cd examples/pylons && ${PREFIX}/bin/python setup.py install ) || exit 1
+( cd examples/pylons/hewwolord && ${PREFIX}/bin/python setup.py install ) || exit 1
 
 echo
 echo All rebuilt and reinstalled.
